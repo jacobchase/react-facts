44c01f26815c3390f0c63710e471eeae9b918586
Jacob Chase <jacobchase7@outlook.com>
2025-04-16 11:01:20 -0500

    initial commit of files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Main from "./components/maincomponent"
+import Navbar from "./components/navbar"
+
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <Main />
+        </>
+    )
+}
---
+++ src/components/maincomponent.jsx
@@ -0,0 +1,5 @@
+export default function MainComponent(){
+    return (
+        <h1>Main component goes here</h1>
+    )
+}
